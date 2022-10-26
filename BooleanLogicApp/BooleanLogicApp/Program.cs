4ef55d3d61094d567d3d68149c9ec9cf47cff412
Keith Moore <keithmoore13@gmail.com>
2022-10-25 20:51:04 -0600

    pushing to main

---
+++ BooleanLogicApp/BooleanLogicApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace BooleanLogicApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // FIguring out whether someone can be approved for car insurance
+            // Asking main questions
+            Console.WriteLine("What is your age?");
+            string Age = Console.ReadLine();
+            int theirAge = Convert.ToInt32(Age);
+            Console.WriteLine("Have you never had a DUI? Reply \"True\" or \"False\".");
+            string statusDUI = Console.ReadLine();
+            bool DUI = Convert.ToBoolean(statusDUI);
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedingTickets = Console.ReadLine();
+            int ticketsAccumulated = Convert.ToInt32(speedingTickets);
+            Console.WriteLine("Qualified?");
+            bool qualified = (theirAge > 15 && DUI && ticketsAccumulated <= 3 );
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
