e8b936c04035e6420b7d38492c7d97503c6c30aa
Keith Moore <keithmoore13@gmail.com>
2022-11-01 16:28:27 -0600

    pushing to main

--- BooleanLogicApp/BooleanLogicApp/Program.cs
+++ BooleanLogicApp/BooleanLogicApp/Program.cs
@@ -12,14 +12,14 @@ namespace BooleanLogicApp
             Console.WriteLine("What is your age?");
             string Age = Console.ReadLine();
             int theirAge = Convert.ToInt32(Age);
-            Console.WriteLine("Have you never had a DUI? Reply \"True\" or \"False\".");
+            Console.WriteLine("Have you ever had a DUI? Reply \"True\" or \"False\".");
             string statusDUI = Console.ReadLine();
             bool DUI = Convert.ToBoolean(statusDUI);
             Console.WriteLine("How many speeding tickets do you have?");
             string speedingTickets = Console.ReadLine();
             int ticketsAccumulated = Convert.ToInt32(speedingTickets);
             Console.WriteLine("Qualified?");
-            bool qualified = (theirAge > 15 && DUI && ticketsAccumulated <= 3 );
+            bool qualified = (theirAge > 15 && !DUI && ticketsAccumulated <= 3 );
             Console.WriteLine(qualified);
             Console.ReadLine();
 
