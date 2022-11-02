31f08e7b1dfa5ad70d304bab566f76bc4a9b3900
Keith Moore <keithmoore13@gmail.com>
2022-11-01 19:20:32 -0600

    pushing to main

--- PriceQuoteApp/PriceQuoteApp/Program.cs
+++ PriceQuoteApp/PriceQuoteApp/Program.cs
@@ -13,24 +13,40 @@ namespace PriceQuoteApp
             Console.WriteLine("Please enter the weight of your package:");
             string packageEntry = Console.ReadLine();
             int packageWeight = Convert.ToInt32(packageEntry);
-            string result = packageWeight > 50 ? "Package too heavy to be shipped via Package Express. Have a good day." : "Please enter the width of your package below:";
-            Console.WriteLine(result);           
-            string packageWdth = Console.ReadLine();
-            int packageWidth = Convert.ToInt32(packageWdth);
-            Console.WriteLine("Please enter the height of your package:");
-            string packageHt = Console.ReadLine();
-            int packageHeight = Convert.ToInt32(packageHt);
-            Console.WriteLine("Please enter the length of your package:");
-            string packageLgth = Console.ReadLine();
-            int packageLength = Convert.ToInt32(packageLgth);
-            // Calculating final price and displaying it to the user
-            double finalPrice = (packageLength * packageHeight * packageWidth) / packageWeight;
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
 
-            Console.WriteLine("Your estimated total for shipping this package is: $" + finalPrice);
-            Console.WriteLine("Thank you!");
-            Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please enter the width of your package:");
+                string packageWdth = Console.ReadLine();
+                int packageWidth = Convert.ToInt32(packageWdth);
+                Console.WriteLine("Please enter the height of your package:");
+                string packageHt = Console.ReadLine();
+                int packageHeight = Convert.ToInt32(packageHt);
+                Console.WriteLine("Please enter the length of your package:");
+                string packageLgth = Console.ReadLine();
+                int packageLength = Convert.ToInt32(packageLgth);
+                int packageSize = packageLength + packageHeight + packageWidth;
+                if (packageSize > 50)
+                {
+                    Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    // Calculating final price and displaying it to the user
+                    double finalPrice = (packageLength * packageHeight * packageWidth) * packageWeight / 100;
 
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + finalPrice);
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                }
 
+            }           
 
         }
     }
