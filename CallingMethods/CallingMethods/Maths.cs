a7986316a3650887a01868677e408edbbca7e2a6
Keith Moore <keithmoore13@gmail.com>
2022-11-04 21:49:15 -0600

    pushing to main

---
+++ CallingMethods/CallingMethods/Maths.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Maths
+    {
+        public static void fiveMethod(int userEntry)
+        {
+            int plusFive = userEntry + 5;
+            Console.WriteLine("Your number plus 5 is " + plusFive);
+            Console.ReadLine();
+        }
+
+        public static void sixMethod(int userEntry)
+        {
+            int plusSix = userEntry + 6;
+            Console.WriteLine("Your number plus 6 is " + plusSix);
+            Console.ReadLine();
+        }
+
+        public static void sevenMethod(int userEntry)
+        {
+            int plusSeven = userEntry + 7;
+            Console.WriteLine("Your number plus 7 is " + plusSeven);
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Maths math = new Maths();
+            //math
+
+            Console.WriteLine("Select a number:");
+            string Entry = Console.ReadLine();
+            int userEntry = Convert.ToInt32(Entry);
+            Maths.fiveMethod(userEntry);
+            Maths.sixMethod(userEntry);
+            Maths.sevenMethod(userEntry);
+
+
+
+        }
+    }
+}
