f2c0159c611e1aae48ef4165c88788f4d3d9e367
Keith Moore <keithmoore13@gmail.com>
2022-11-07 17:51:12 -0700

    pushing to main

---
+++ MethodOverloading/MethodOverloading/Maths.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Maths
+    {
+        // creating three methods that have the userEntry passed in
+        public int mathMethod(int userEntry)
+        {
+            int answer = userEntry + 5;
+            return answer;
+        }
+
+        public decimal mathMethod(decimal userEntry2)
+        {
+            decimal round = Decimal.Round(userEntry2);
+            int converted = Convert.ToInt32(round);
+            int answer2 = converted + 5;
+            return answer2;
+        }
+
+        public int mathMethod(string Entry3)
+        {
+            int converted = Convert.ToInt32(Entry3);
+            int answer3 =  converted + 5;
+            return answer3;
+        }
+    }
+}
---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating the class
+            Maths maths = new Maths();
+            //asking user for 3 inputs, an int, decimal, and string, and then calling the methods
+            //from the Maths class that use those inputs. 
+            Console.WriteLine("Select a number:");
+            string Entry = Console.ReadLine();
+            int userEntry = Convert.ToInt32(Entry);
+            Console.WriteLine("Provide a decimal number:");
+            string Entry2 = Console.ReadLine();
+            decimal userEntry2 = Convert.ToDecimal(Entry2);
+            Console.WriteLine("Select another number:");
+            string Entry3 = Console.ReadLine();
+            //int userEntry3 = Convert.ToInt32(Entry3);
+
+            Console.WriteLine(maths.mathMethod(userEntry));
+            Console.WriteLine(maths.mathMethod(userEntry2));
+            Console.WriteLine(maths.mathMethod(Entry3));
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
