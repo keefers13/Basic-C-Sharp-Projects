871c2016cf19afc433736efcb4cb1e60a48a1256
Keith Moore <keithmoore13@gmail.com>
2022-11-08 16:32:23 -0700

    pushing to main

---
+++ ClassInstantiation/ClassInstantiation/Maths.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInstantiation
+{
+    public class Maths
+    {
+        public void myMethod(int firstEntry,int  secondEntry)
+        {
+            int answer = firstEntry + 5;
+            Console.WriteLine("This was the second number: " + secondEntry);
+        }
+    }
+}
---
+++ ClassInstantiation/ClassInstantiation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInstantiation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Maths maths = new Maths();
+
+            Console.WriteLine("Select a number:");
+            string Entry = Console.ReadLine();
+            int firstEntry = Convert.ToInt32(Entry);
+            Console.WriteLine("Select another number:");
+            string Entry2 = Console.ReadLine();
+            int secondEntry = Convert.ToInt32(Entry2);
+
+            maths.myMethod(firstEntry, secondEntry);
+            maths.myMethod(firstEntry: 5, secondEntry: 6);
+            Console.ReadLine();
+
+        }
+    }
+}
