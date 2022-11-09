5ed3147cc668a17a059069f5a9ca5beeb8a379a5
Keith Moore <keithmoore13@gmail.com>
2022-11-08 17:27:47 -0700

    pushing to main

---
+++ VoidMethod/VoidMethod/Maths.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class Maths
+    {
+        public void myMethod(int Entry1, out int answer)
+        {
+            answer = Entry1 / 2;
+        }
+        public void myMethod(string name)
+        {
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public static class Program
+    {
+        //made this method static
+        public static void Main(string[] args)
+        {
+            //instantiating the method from class Maths
+            Maths maths = new Maths();
+
+            //asking for user input
+            Console.WriteLine("Select a number:");
+            string Entry = Console.ReadLine();
+            int Entry1 = Convert.ToInt32(Entry);
+
+            //displaying the user input to the screen, divided by two
+            maths.myMethod(Entry1, out int answer);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+             //asking for user input
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            //calling on an overloaded method to display user input
+            maths.myMethod(name);
+
+
+        }
+    }
+}
