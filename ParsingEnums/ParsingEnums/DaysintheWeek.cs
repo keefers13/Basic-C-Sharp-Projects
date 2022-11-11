69f967b8dcdf23eeae4d0ef8c4e0c3f00f1dbb8a
Keith Moore <keithmoore13@gmail.com>
2022-11-10 18:23:16 -0700

    pushing to main

---
+++ ParsingEnums/ParsingEnums/DaysintheWeek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    public class DaysintheWeek
+    {
+        //creating the property Day
+        public DaysoftheWeek Day { get; set; }
+    }
+
+    public enum DaysoftheWeek //Creating enum for the days and assigning a set value
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7,
+    }
+}
---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a day of the week (example \"Monday\")");
+            string daySelected = Console.ReadLine();
+
+            try //try/catch block that runs the code and ensures correct inupt
+            {
+                //Assigning the value to the variable of the enum data type 
+                
+                DaysoftheWeek day = (DaysoftheWeek)Enum.Parse(typeof(DaysoftheWeek), daySelected); 
+                // ensuring user input is part of the data defined in the enum 
+                if (Enum.IsDefined(typeof(DaysoftheWeek), day))
+                {
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                }
+
+            }
+
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+
+
+
+}
