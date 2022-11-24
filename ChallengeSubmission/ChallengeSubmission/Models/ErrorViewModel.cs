369966b37dcbc7a7457d46d0d98b512ddec72c6e
Keith Moore <keithmoore13@gmail.com>
2022-11-23 17:10:06 -0700

    push to main

---
+++ ChallengeSubmission/ChallengeSubmission/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChallengeSubmission.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
